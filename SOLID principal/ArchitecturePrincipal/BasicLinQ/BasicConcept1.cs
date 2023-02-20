ed8dfc4a0b3e29776f0c8f843dfeeb6b2c4df889
prakash jamer <prakashjamer@hotmail.com>
2023-02-21 00:09:39 +0530

    BasicConcept 1AND 2

---
+++ SOLID principal/ArchitecturePrincipal/BasicLinQ/BasicConcept1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLinQ
+{
+    internal class BasicConcept1
+    {
+
+        /// Tesrt Chabges 
+        /// test 2
+        /// // tewsr  staah 3
+    }
+}
